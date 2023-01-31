5dc82edf45b93296303d9e5ec4260c4cc50d2dce
ashwinidube1 <122586080+ashwinidube1@users.noreply.github.com>
2023-01-31 14:17:35 +1300

    Update info.php

--- info.php
+++ info.php
@@ -1,1 +1,3 @@
-
+<?php $firstname = isset($_POST['firstname'] : $_POST['firstname'] : null; $lastname = isset($_POST['lastname'] : $_POST['lastname'] : null; $email = isset($_POST['email'] : $_POST['email'] : null;
+$db = new mysqli("localhost", "username", "password", "database"); if ($db->errno) die("Error opening database: " . $db->error());
+$query = 'INSERT INTO users (firstname, lastname, email) VALUES (?, ?, ?)'; $result = $db->prepare($query); $result->bind_param('sss', $firstname, $lastname, $email); $result->execute(); free($result); ?>
